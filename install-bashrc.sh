e741348f17bcc430ba8e43252e973e1d96fe2bcc
Wilhelmina Drengwitz <ghthor@gmail.com>
2015-07-03 11:44:48 -0400

    Fix the install script to use symbolic links

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cp bashrc $HOME/.bashrc
-cp bash_aliases $HOME/.bash_aliases
-cp bash_funcs $HOME/.bash_funcs
+ln -s "$1/profile"      $HOME/.profile
+ln -s "$1/bash_profile" $HOME/.bash_profile
+ln -s "$1/bashrc"       $HOME/.bashrc
+ln -s "$1/bash_aliases" $HOME/.bash_aliases
+ln -s "$1/bash_funcs"   $HOME/.bash_funcs
+ln -s "$1/inputrc"      $HOME/.inputrc
