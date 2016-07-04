415654a687cc97a2af261f2ca96db70fea921933
Wilhelmina Drengwitz <ghthor@gmail.com>
2016-07-03 21:57:42 -0400

    Remove deprecated install script

--- install-bashrc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s "$1/profile"      $HOME/.profile
-ln -s "$1/bash_profile" $HOME/.bash_profile
-ln -s "$1/bashrc"       $HOME/.bashrc
-ln -s "$1/bash_aliases" $HOME/.bash_aliases
-ln -s "$1/bash_funcs"   $HOME/.bash_funcs
-ln -s "$1/inputrc"      $HOME/.inputrc
